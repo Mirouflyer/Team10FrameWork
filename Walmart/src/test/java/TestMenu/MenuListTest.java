537d0aaf4af67e51ebc039a15b7f8ca872134053
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-24 23:36:08 -0400

    dataBase,MenuTest,HomePageTest

---
+++ Walmart/src/test/java/TestMenu/MenuListTest.java
@@ -0,0 +1,13 @@
+package TestMenu;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+import pages.MenuPage;
+
+public class MenuListTest extends CommonAPI {
+
+    @Test
+    public void listSearch() throws Exception{
+        MenuPage.readMenuText(driver);
+    }
+}
