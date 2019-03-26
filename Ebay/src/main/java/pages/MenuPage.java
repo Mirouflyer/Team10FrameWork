d83741cc64e13d2544da06671355b67bd3f6308d
hargasyoucef <hargasyoucef@gmail.com>
2019-03-25 20:44:44 -0400

    Added MenuPage, SearchPage

---
+++ Ebay/src/main/java/pages/MenuPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import base.CommonAPI;
+import database.ConnectToSqlDB;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuPage {
+    public static String filePath = "C:\\Users\\Youcef\\Desktop\\Samir Final Test\\Team10FrameWork\\Ebay\\src\\test\\resources\\secret.properties";
+
+    public static void readMenuText(WebDriver driver1)throws Exception, IOException, SQLException, ClassNotFoundException{
+
+        List<String> actualData = new ArrayList<>();
+        List<String> listOfMenu = CommonAPI.getTextFromWebElements("#gh-cat-box");
+        SearchPage searchPage = PageFactory.initElements(driver1, SearchPage.class);
+        searchPage.searchItemsAndSubmitButton(driver1);
+
+        //To Be implemented, to read from database
+        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+        connectToSqlDB.insertDataFromArrayListToSqlTable(listOfMenu, "itemList1" , "items", filePath);
+        actualData = connectToSqlDB.readDataBase("itemList1","items", filePath );
+        for (String data:actualData){
+            System.out.println(data);
+        }
+        List<String> menuData = new ArrayList<>();
+        Assert.assertEquals(menuData,actualData);
+    }
+}
