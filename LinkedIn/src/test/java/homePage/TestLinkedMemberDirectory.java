8651f635da3b781a792e434eb362fbcae0e01c48
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-26 22:42:49 -0400

    FindaCollegue and LinkedInMember Test

---
+++ LinkedIn/src/test/java/homePage/TestLinkedMemberDirectory.java
@@ -0,0 +1,20 @@
+package homePage;
+
+import base.CommonAPI;
+import homePageLinks.LinkedMemberDirectory;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import reporting.ApplicationLog;
+import reporting.TestLogger;
+import sun.rmi.rmic.iiop.PrintGenerator;
+
+public class TestLinkedMemberDirectory extends CommonAPI {
+
+    @Test
+    public void linkedMemberDirectory() throws Exception{
+        ApplicationLog.epicLogger();
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        LinkedMemberDirectory linkedMemberDirectory = PageFactory.initElements(driver,LinkedMemberDirectory.class);
+        linkedMemberDirectory.clickOnLinkedMemberDirectoryWebElement();
+    }
+}
