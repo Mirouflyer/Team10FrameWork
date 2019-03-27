3f27b9ec658b182035462a84ff4ce00f51bb180d
hargasyoucef <hargasyoucef@gmail.com>
2019-03-26 21:50:56 -0400

    Added SearchItemByPrice, TestItemByPrice

---
+++ Ebay/src/test/java/advancedSearch/TestSearchItemByPrice.java
@@ -0,0 +1,19 @@
+package advancedSearch;
+
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import reporting.ApplicationLog;
+import reporting.TestLogger;
+import searchItem.SearchItemByPrice;
+
+public class TestSearchItemByPrice extends CommonAPI {
+
+    @Test
+    public void FindItemUsingPriceOption() throws InterruptedException {
+        ApplicationLog.epicLogger();
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+        SearchItemByPrice searchItemByPrice = PageFactory.initElements(driver, SearchItemByPrice.class);
+        searchItemByPrice.findItemByPrice();
+    }
+}
