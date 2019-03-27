ed84104096b5f8ef25c55158b2f55f5a6a897fce
ferhat83 <saadiferhatsaid@gmail.com>
2019-03-27 18:43:01 -0400

    add  to menu

--- CNN/src/main/java/menu/CnnMenu.java
+++ CNN/src/main/java/menu/CnnMenu.java
@@ -13,11 +13,24 @@ public class CnnMenu extends CommonAPI {
     public static WebElement food;
     @FindBy(xpath = "//a[@class='nav-flyout__submenu-link'][contains(text(),'Fitness')]  ")
     public static WebElement fitness;
+    @FindBy(xpath = "//div[@class='nav-flyout__menu-item nav-flyout__menu-item--health']//a[@class='nav-flyout__submenu-link'][contains(text(),'Wellness')] ")
+    public static WebElement wellness;
+    @FindBy(xpath = "//a[@class='nav-flyout__submenu-link'][contains(text(),'Parenting')]  ")
+    public static WebElement parenting;
+    @FindBy(xpath = "//a[@class='nav-flyout__submenu-link'][contains(text(),'Live Longer')]  ")
+    public static WebElement liveLonger;
+    @FindBy(xpath = "//a[@class='nav-flyout__submenu-link'][contains(text(),'Pro Football')] ")
+    public static WebElement proFootball;
 
     //getters
     public static WebElement getMenu() { return menu; }
     public static WebElement getFood() { return food; }
-    public static WebElement getFitness() { return fitness; }
+    public static WebElement getFitness() { return fitness;}
+    public static WebElement getWellness(){return wellness;}
+    public static WebElement getParenting() { return parenting; }
+    public static WebElement getLiveLonger() { return liveLonger; }
+    public static WebElement getProFootball() { return proFootball; }
+
 
     //runMethods
     public void clickOnMenu() {
@@ -35,6 +48,30 @@ public class CnnMenu extends CommonAPI {
         getFitness().click();
         driver.navigate().back();
     }
+    public void clickOnWellness() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+        getMenu().click();
+        getWellness().click();
+        driver.navigate().back();
+    }
+    public void clickOnParenting() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+        getMenu().click();
+        getParenting().click();
+        driver.navigate().back();
+    }
+    public void clickOnLiveLonger (){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+        getMenu().click();
+        getLiveLonger().click();
+        driver.navigate().back();
+    }
+    public void clickOnProFootball (){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
+        getMenu().click();
+        getProFootball().click();
+        driver.navigate().back();
+    }
 
     //useRunMethods
     public void useRunMethods(){
@@ -42,6 +79,10 @@ public class CnnMenu extends CommonAPI {
        clickOnMenu();
        clickOnFood();
        clickOnFitness();
+       clickOnWellness();
+       clickOnParenting();
+       clickOnLiveLonger();
+       clickOnProFootball();
     }
 
-    }
+}
